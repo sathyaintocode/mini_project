c65ca442f022692df8cfb9dab449d1ec0088242e
SATHYABHAMA SHIBU <77277041+sathyaintocode@users.noreply.github.com>
2024-09-11 08:03:22 +0530

    to detect the environment temperature and humidity

---
+++ temp.py
@@ -0,0 +1,15 @@
+import Adafruit_DHT
+import time
+
+DHT_SENSOR = Adafruit_DHT.DHT11
+DHT_PIN = 21
+
+def sensor_dht11():
+    humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
+    
+    if humidity is not None and temperature is not None:
+        print("Temp={0:0.1f}C Humidity={1:0.1f}%".format(temperature, humidity))
+    else:
+        print("Sensor failure. Check wiring.")
+    time.sleep(1)
+    return humidity, temperature
